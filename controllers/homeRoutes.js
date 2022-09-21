ae81750ac27b4055af22abcf5168926197829d17
Mountian Driver <lsfreigenberg@outlook.com>
2022-09-20 21:29:13 -0600

    fixed bugs and frontend connected

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,6 +1,31 @@
 const router = require('express').Router();
-const { Recipe, User } = require('../../models');
-const withAuth = require('../../utils/auth');
+const { Recipe, User } = require('../models');
+const withAuth = require('../utils/auth');
+
+router.get('/', async (req, res) => {
+  try {
+    // Get all projects and JOIN with user data
+    const recipeData = await Recipe.findAll({
+      include: [
+        {
+          model: User,
+          attributes: ['name'],
+        },
+      ],
+    });
+
+    // Serialize data so the template can read it
+    // const projects = projectData.map((project) => project.get({ plain: true }));
+
+    // Pass serialized data and session flag into template
+    res.render('homepage', { 
+      // projects, 
+      // logged_in: req.session.logged_in 
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+})
 
 router.post('/', withAuth, async (req, res) => {
   try {
